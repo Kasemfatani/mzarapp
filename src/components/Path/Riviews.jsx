60fba32459a76a42aa3e0475c7ecd8aa8ffaac5b
AhmedAbdullah125 <ahmednabeeh98@gmail.com>
2025-02-09 22:13:42 +0200

    update

--- src/components/Path/Riviews.jsx
+++ src/components/Path/Riviews.jsx
@@ -1,10 +1,20 @@
 'use client'
 import React, { useEffect, useState } from 'react';
 import SingleNews from '../../components/blog/SingleNews';
+import axios from 'axios';
+import { API_BASE_URL } from '@/lib/apiConfig';
+import Loading from '@/app/loading';
+
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
 export default function Riviews({ id }) {
 
     const [loading, setLoading] = useState(true); // State for loading indicator
-    const [data, setData] = useState(null);
+    const [data, setData] = useState([]);
     const [error, setError] = useState(null);
     const [language, setLanguage] = useState('en');  // Default language is 'en'
 
@@ -17,25 +27,87 @@ export default function Riviews({ id }) {
                 lang: localStorage.getItem('lang'), // Change language dynamically based on state
             };
             // Fetch data from the API with Axios
-            // axios.get(`${API_BASE_URL}/landing/home/packages/details?package_id=${pathId}`
-            //     , {
-            //         headers: headers,
-            //     })
-            //     .then(response => {
-            //         setData(response.data);  // Set the response data to state
-            //         setLoading(false);  // Set loading to false
+            axios.get(`${API_BASE_URL}/landing/home/packages/reviews?package_id=${id}`
+                , {
+                    headers: headers,
+                })
+                .then(response => {
+                    setData(response.data.data);  // Set the response data to state
+                    setLoading(false);  // Set loading to false
 
-            //     })
-            //     .catch(error => {
-            //         setError(error);  // Handle any errors
-            //         console.error('Error fetching data:', error);
-            //         setLoading(false)
-            //     });
+                })
+                .catch(error => {
+                    setError(error);  // Handle any errors
+                    console.error('Error fetching data:', error);
+                    setLoading(false)
+                });
         }
     }, []);  // Run this effect whenever the `language` changes
+    console.log(data);
+
     return (
-        <div className="blog" style={{ direction: language === 'en' ? 'ltr' : 'rtl' }}>
-          
-        </div>
+        <>
+            {
+                data.length > 0 ?
+                    <div className="container m-auto" style={{ direction: `${language === 'ar' ? 'rtl' : 'ltr'}` }}>
+                        <h4 className='t-title-review'>{language === 'en' ? 'Reviews' : 'التقييمات'}</h4>
+                        {
+                            loading ? <Loading /> :
+                                <div className="reviews-swiper w-full" style={{ direction: `ltr` }}>
+                                    <Swiper
+                                        dir={language === 'ar' ? 'rtl' : 'ltr'}
+                                        // cssMode={language === 'ar'} // Important for RTL support
+                                        spaceBetween={32}
+                                        slidesPerView={7.5}
+                                        autoplay={true}
+                                        loop={true}
+                                        navigation={true}
+                                        modules={[Autoplay, Navigation, Pagination]}
+                                        breakpoints={{
+                                            1400: {
+                                                slidesPerView: 3,
+                                            },
+                                            1100: {
+                                                slidesPerView: 3,
+                                            },
+                                            767: {
+                                                slidesPerView: 2,
+                                            },
+                                            768: {
+                                                slidesPerView: 2,
+                                                autoplay: false,
+                                            },
+                                            640: {
+                                                slidesPerView: 1.2,
+                                                autoplay: false,
+                                                spaceBetween: 16
+                                            },
+                                            100: {
+                                                slidesPerView: 1.2,
+                                                autoplay: false,
+                                                spaceBetween: 16
+                                            }
+                                        }}
+                                    >
+                                        {data?.map((review) =>
+                                            <SwiperSlide key={review.id}>
+                                                <div className="review-cont">
+                                                    <h4>{review.title}</h4>
+                                                    <div className="stars">
+                                                        {[...Array(5)].map((star, index) => (
+                                                            <i class={`${review.rating > index ? 'fa-solid' : 'fa-regular'} fa-star ${review.rating > index ? 'active' : ''}`}></i>
+                                                        ))}
+                                                    </div>
+                                                    <p>{review.description}</p>
+                                                </div>
+                                            </SwiperSlide>
+                                        )}
+                                    </Swiper>
+                                </div>
+                        }
+                    </div>
+                    : null
+            }
+        </>
     );
 }
