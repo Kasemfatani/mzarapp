298880709b8e8b1cb385f5d463185b98b9f0efaf
USMAN-SENG <othmandoloh@gmail.com>
2025-08-18 23:42:22 +0700

    test

--- src/app/book-path/page.jsx
+++ src/app/book-path/page.jsx
@@ -6,8 +6,8 @@ import bg from "/public/bg.png";
 export default function Book() {
 	return (
 		<div className="w-full min-h-screen flex items-center justify-center">
-				{/* <FormsWrapper /> */}
-				<FormsWrapperNew />
+				<FormsWrapper />
+				{/* <FormsWrapperNew /> */}
 		</div>
 	);
 }
---
+++ src/app/book/page-new.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import GeneralFormsWrapper from "../../components/book/GeneralFormsWrapper";
+
+export default function Book() {
+	return (
+		<div className="w-full min-h-screen flex items-center justify-center">
+				<GeneralFormsWrapper />
+		</div>
+	);
+}
