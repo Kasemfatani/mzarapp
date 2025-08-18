043938b6e77fe2deaee9dc7667d1762e25c81648
USMAN-SENG <othmandoloh@gmail.com>
2025-08-18 21:33:26 +0700

    try raslania

--- src/app/raslania/page.tsx
+++ src/app/raslania/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { HeroSection } from './components/HeroSection';
 import RegisterApp from './components/RegisterApp';
 import PromoCode from './components/PromoCode';
--- src/app/saad-alqurashi/page.tsx
+++ src/app/saad-alqurashi/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { HeroSection } from './components/HeroSection';
 import RegisterApp from './components/RegisterApp';
 import PromoCode from './components/PromoCode';
