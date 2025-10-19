7ac28f23e694234f60f24581b251046e052787a0
USMAN-SENG <othmandoloh@gmail.com>
2025-10-19 19:28:03 +0700

    comment out time of offer end  since we not using it

--- src/components/Path/Offer.jsx
+++ src/components/Path/Offer.jsx
@@ -1,61 +1,68 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import "@fancyapps/ui/dist/fancybox/fancybox.css";
+//import "@fancyapps/ui/dist/fancybox/fancybox.css";
 
 export default function Offer({ offerEndDate }) {
 	const [language, setLanguage] = useState("en");
-	const [timeLeft, setTimeLeft] = useState({
-		days: 0,
-		hours: 0,
-		minutes: 0,
-		seconds: 0,
-	});
-	const [offerEnded, setOfferEnded] = useState(false);
-
-	function isEven(number) {
-		return number % 2 === 0;
-	}
-
-	const today = new Date();
-	const year = today.getFullYear(); // Get current year
-	const month = String(today.getMonth() + 1).padStart(2, "0"); // Get current month (0-based index)
-	const end = isEven(today.getDate())
-		? today.getDate() + 1
-		: today.getDate() + 2;
-	const formattedEnd = String(end).padStart(2, "0"); // Ensure double-digit format for day
 
 	useEffect(() => {
-		if (typeof window !== "undefined") {
-			setLanguage(localStorage.getItem("lang") || "en");
-		}
+					if (typeof window !== 'undefined') {
+							setLanguage(localStorage.getItem('lang'));
+					}
+			}, []); 
 
-		const openingDate = offerEndDate
-			? new Date(offerEndDate.replace(" ", "T") + "Z").getTime()
-			: new Date(`${year}-${month}-${formattedEnd}T00:00:00Z`).getTime();
+	// const [timeLeft, setTimeLeft] = useState({
+	// 	days: 0,
+	// 	hours: 0,
+	// 	minutes: 0,
+	// 	seconds: 0,
+	// });
+	// const [offerEnded, setOfferEnded] = useState(false);
 
-		const timer = setInterval(() => {
-			const now = new Date().getTime();
-			const difference = openingDate - now;
+	// function isEven(number) {
+	// 	return number % 2 === 0;
+	// }
 
-			if (difference <= 0) {
-				clearInterval(timer);
-				setTimeLeft({ days: 0, hours: 0, minutes: 0, seconds: 0 });
-				setOfferEnded(true);
-			} else {
-				setTimeLeft({
-					days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-					hours: Math.floor(
-						(difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-					),
-					minutes: Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)),
-					seconds: Math.floor((difference % (1000 * 60)) / 1000),
-				});
-				setOfferEnded(false);
-			}
-		}, 1000);
+	// const today = new Date();
+	// const year = today.getFullYear(); // Get current year
+	// const month = String(today.getMonth() + 1).padStart(2, "0"); // Get current month (0-based index)
+	// const end = isEven(today.getDate())
+	// 	? today.getDate() + 1
+	// 	: today.getDate() + 2;
+	// const formattedEnd = String(end).padStart(2, "0"); // Ensure double-digit format for day
+  // comment out because we not using it for now 
+	// useEffect(() => {
+	// 	if (typeof window !== "undefined") {
+	// 		setLanguage(localStorage.getItem("lang") || "en");
+	// 	}
 
-		return () => clearInterval(timer);
-	}, [offerEndDate]);
+	// 	const openingDate = offerEndDate
+	// 		? new Date(offerEndDate.replace(" ", "T") + "Z").getTime()
+	// 		: new Date(`${year}-${month}-${formattedEnd}T00:00:00Z`).getTime();
+
+	// 	const timer = setInterval(() => {
+	// 		const now = new Date().getTime();
+	// 		const difference = openingDate - now;
+
+	// 		if (difference <= 0) {
+	// 			clearInterval(timer);
+	// 			setTimeLeft({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+	// 			setOfferEnded(true);
+	// 		} else {
+	// 			setTimeLeft({
+	// 				days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+	// 				hours: Math.floor(
+	// 					(difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+	// 				),
+	// 				minutes: Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)),
+	// 				seconds: Math.floor((difference % (1000 * 60)) / 1000),
+	// 			});
+	// 			setOfferEnded(false);
+	// 		}
+	// 	}, 1000);
+
+	// 	return () => clearInterval(timer);
+	// }, [offerEndDate]);
 
 	return (
 		<div className="uper-header-counter-cont">
@@ -64,7 +71,7 @@ export default function Offer({ offerEndDate }) {
 					? "Book Now And Get 20% Off"
 					: "احجز الآن واحصل على خصم 20٪"}
 			</span>
-			{!offerEnded && (
+			{/* {!offerEnded && (
 				<div className="uper-header-counter">
 					<div className="days">
 						<span>{timeLeft.days}</span>
@@ -86,7 +93,7 @@ export default function Offer({ offerEndDate }) {
 						<p>{language === "en" ? "Seconds" : "ثواني"}</p>
 					</div>
 				</div>
-			)}
+			)} */}
 		</div>
 	);
 }
