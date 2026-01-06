8ae626f541232dfc115d714f443e9ba74c8c8b0b
USMAN SENG <61685876+USMAN-SENG@users.noreply.github.com>
2026-01-06 19:37:18 +0700

    small change in blog

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,13 +1,21 @@
 import { cookies, headers } from "next/headers";
-// import { notFound } from "next/navigation";
+import { notFound } from "next/navigation";
 // import { API_BASE_URL } from "@/lib/apiConfig";
 import SingleBlogWrapper from "@/components/blog-new/SingleBlogWrapper";
 
-// import { API_BASE_URL_NEW } from "@/lib/apiConfig";
+import { BLOG_URL } from "@/lib/apiConfig";
 
-// import { cache } from "react";
+import { cache } from "react";
 
+const getData = cache(async (lang, slug) => {
+	const res = await fetch(`${BLOG_URL}/api/blogs/${slug}`, {
+		headers: { lang },
+	});
 
+	if (!res.ok) return null;
+	const json = await res.json();
+	return json?.data || null;
+});
 
 // Helper to determine language (keep this logic centralized)
 function determineLang() {
@@ -19,28 +27,35 @@ function determineLang() {
 }
 
 export function generateMetadata() {
-	
 	const lang = determineLang();
 
 	if (lang === "ar") {
 		return {
 			title: "مقال",
-			
 		};
 	}
 	return {
 		title: "Blog",
-		
 	};
 }
 
-export default async function Page() {
-	
+export default async function Page({ params }) {
+	const { slug } = params;
 
 	const lang = determineLang();
 
+	const blogsData = await getData(lang, slug);
+
+	if (!blogsData) {
+		notFound();
+	}
 
 	return (
-		<SingleBlogWrapper lang={lang} />
+		<SingleBlogWrapper
+			lang={lang}
+			blog={blogsData}
+			BLOG_URL={BLOG_URL}
+			slug={slug}
+		/>
 	);
 }
--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -1,12 +1,25 @@
 import { cookies, headers } from "next/headers";
-// import { notFound } from "next/navigation";
+import { notFound } from "next/navigation";
 // import { API_BASE_URL } from "@/lib/apiConfig";
 import BlogWrapper from "@/components/blogs/BlogWrapper";
 
-// import { API_BASE_URL_NEW } from "@/lib/apiConfig";
+import { BLOG_URL } from "@/lib/apiConfig";
 
-// import { cache } from "react";
+import { cache } from "react";
 
+const getData = cache(async (lang) => {
+	
+	const res = await fetch(
+		`${BLOG_URL}/api/blogs`,
+		{
+			headers: { lang },
+		}
+	);
+
+	if (!res.ok) return null;
+	const json = await res.json();
+	return json?.data || null;
+});
 
 
 // Helper to determine language (keep this logic centralized)
@@ -39,8 +52,14 @@ export default async function Page() {
 
 	const lang = determineLang();
 
+	const blogsData = await getData(lang);
+
+	if (!blogsData) {
+		notFound();
+	} 
+
 
 	return (
-		<BlogWrapper lang={lang} />
+		<BlogWrapper lang={lang} blogsData={blogsData} BLOG_URL={BLOG_URL} />
 	);
 }
