bfa477f982a63fb59a83c412c08a632e859287b4
USMAN SENG <61685876+USMAN-SENG@users.noreply.github.com>
2026-01-18 19:14:54 +0700

    add hero img to review

--- src/components/reviews/PageHeader.jsx
+++ src/components/reviews/PageHeader.jsx
@@ -1,18 +1,30 @@
+import Image from "next/image";
 
 export function PageHeader({ isAr }) {
-  return (
-    <section className="bg-gradient-to-b from-[#f5f2ed] to-white py-16 md:py-20" >
-      <div className="container mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="text-center">
-          <h1 className="mb-6 text-4xl text-[#3C6652] md:text-5xl lg:text-6xl">
-          {isAr ? "استعرض جميع التقييمات" : "View All Reviews"}
-          </h1>
-          <p className="mx-auto max-w-3xl text-xl leading-relaxed text-[#718096] md:text-2xl">
-            {isAr ? "اطّلع على آراء وتجارب زوّار مزار بعد خوض الجولات والخدمات السياحية المعتمدة." : "Check out the opinions and experiences of Mazah visitors after taking the approved tours and tourist services."}
-          </p>
-          <div className="mx-auto mt-8 h-px w-32 bg-gradient-to-l from-transparent via-[#867957] to-transparent" />
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section className="relative bg-gradient-to-b from-[#f5f2ed] to-white py-16 md:py-20">
+			<div className="absolute inset-0 z-0 ">
+				<Image
+					src="/reviews/hero.png"
+					alt="Reviews Background"
+					className="w-full h-full object-cover"
+					fill
+					priority
+				/>
+			</div>
+			<div className="relative container mx-auto max-w-7xl px-6 lg:px-8 z-10">
+				<div className="text-center">
+					<h1 className="mb-6 text-4xl text-white md:text-5xl lg:text-6xl">
+						{isAr ? "استعرض جميع التقييمات" : "View All Reviews"}
+					</h1>
+					<p className="mx-auto max-w-3xl text-xl leading-relaxed text-white/90 md:text-2xl">
+						{isAr
+							? "اطّلع على آراء وتجارب زوّار مزار بعد خوض الجولات والخدمات السياحية المعتمدة."
+							: "Check out the opinions and experiences of Mazah visitors after taking the approved tours and tourist services."}
+					</p>
+					{/* <div className="mx-auto mt-8 h-px w-32 bg-gradient-to-l from-transparent via-[#867957] to-transparent" /> */}
+				</div>
+			</div>
+		</section>
+	);
 }
