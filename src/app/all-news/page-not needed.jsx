db642ba0fc07f116f61019087632fac061be92d4
USMAN SENG <61685876+USMAN-SENG@users.noreply.github.com>
2026-02-01 16:24:46 +0700

    delete some old files

--- src/app/all-news/page-not needed.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import AllNews from '../../components/news/AllNews';
-
-export default function Latest() {
-
-    return (
-        <AllNews />
-    );
-}
--- src/app/blogs-old/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ContentPage from '../../components/home/ContentPage';
-export default function Blog() {
-    return (
-        <ContentPage />
-    );
-}
